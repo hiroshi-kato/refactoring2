a3a59506d965fcc2d3952a292cd5c7a0aff58901
hiroshi-kato <hiroshikato1208@gmail.com>
2019-12-30 09:33:28 +0900

    first example

---
+++ 1/index.test.js
@@ -0,0 +1,47 @@
+const statement = require ('./index');
+
+const invoices = [
+  {
+    customer: 'BigCo',
+    performances: [
+      {
+        playID: 'hamlet',
+        audience: 55,
+      },
+      {
+        playID: 'as-like',
+        audience: 35,
+      },
+      {
+        playID: 'othello',
+        audience: 40,
+      },
+    ],
+  },
+];
+
+const plays = {
+  hamlet: {
+    name: 'Hamlet',
+    type: 'tragedy',
+  },
+  'as-like': {
+    name: 'As You Like It',
+    type: 'comedy',
+  },
+  othello: {
+    name: 'Othello',
+    type: 'tragedy',
+  },
+};
+
+test ('test', () => {
+  expected = `Statement for BigCo
+  Hamlet: $650.00 (55 seats)
+  As You Like It: $580.00 (35 seats)
+  Othello: $500.00 (40 seats)
+Amount owed is $1,730.00
+You earned 47 credits
+`;
+  expect (statement (invoices[0], plays)).toBe (expected);
+});
