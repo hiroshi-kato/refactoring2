1c343207fe52dcc7824d205fc24657f45870a532
hiroshi-kato <hiroshikato1208@gmail.com>
2019-12-30 09:36:26 +0900

    extract function

--- 1/index.js
+++ 1/index.js
@@ -11,26 +11,8 @@ function statement (invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
+    let thisAmount = amountFor (perf, play);
 
-    switch (play.type) {
-      case 'tragedy':
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error (`unknown type: ${play.type}`);
-    }
-    // ボリューム特典のポイントを加算
     volumeCredits += Math.max (perf.audience - 30, 0);
     // 喜劇のときは10人につき、さらにポイントを加算
     if ('comedy' === play.type) volumeCredits += Math.floor (perf.audience / 5);
@@ -40,6 +22,28 @@ function statement (invoice, plays) {
   }
   result += `Amount owed is ${format (totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
+  return result;
+}
+
+function amountFor (perf, play) {
+  let result = 0;
+  switch (play.type) {
+    case 'tragedy':
+      result = 40000;
+      if (perf.audience > 30) {
+        result += 1000 * (perf.audience - 30);
+      }
+      break;
+    case 'comedy':
+      result = 30000;
+      if (perf.audience > 20) {
+        result += 10000 + 500 * (perf.audience - 20);
+      }
+      result += 300 * perf.audience;
+      break;
+    default:
+      throw new Error (`unknown type: ${play.type}`);
+  }
   return result;
 }
 
