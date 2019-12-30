159a4311e754d34eb72b941a003622caa24954d9
hiroshi-kato <hiroshikato1208@gmail.com>
2019-12-30 14:47:03 +0900

    refactor totalAmount

--- 1/index.js
+++ 1/index.js
@@ -1,13 +1,12 @@
 function statement (invoice, plays) {
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
     // 注文の内訳を出力
     result += `  ${playFor (perf).name}: ${usd (amountFor (perf))} (${perf.audience} seats)\n`;
-    totalAmount += amountFor (perf);
   }
-  result += `Amount owed is ${usd (totalAmount)}\n`;
+
+  result += `Amount owed is ${usd (totalAmount ())}\n`;
   result += `You earned ${totalVolumeCredits ()} credits\n`;
   return result;
 
@@ -60,6 +59,14 @@ function statement (invoice, plays) {
     let result = 0;
     for (let perf of invoice.performances) {
       result += volumeCreditsFor (perf);
+    }
+    return result;
+  }
+
+  function totalAmount () {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor (perf);
     }
     return result;
   }
