8c2f35e91803a1e42914a00530d01b44742e2f73
hiroshi-kato <hiroshikato1208@gmail.com>
2019-12-31 18:05:28 +0900

    add renderHtml

--- 1/index.test.js
+++ 1/index.test.js
@@ -1,5 +1,5 @@
 // const statement = require ('./index');
-import statement from './index';
+import statement, {htmlStatement} from './index';
 
 const invoices = [
   {
@@ -36,7 +36,7 @@ const plays = {
   },
 };
 
-test ('test', () => {
+test ('render text', () => {
   const expected = `Statement for BigCo
   Hamlet: $650.00 (55 seats)
   As You Like It: $580.00 (35 seats)
@@ -45,4 +45,17 @@ Amount owed is $1,730.00
 You earned 47 credits
 `;
   expect (statement (invoices[0], plays)).toBe (expected);
+});
+test ('render HTML', () => {
+  const expected = `<h1>Statement for BigCo</h1>
+<table>
+<tr><th>play</th><th>seets</th><th>cost</th></tr>
+  <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>
+  <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>
+  <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>
+</table>
+<p>Amount owed is <em>$1,730.00</em></p>
+<p>You earned <em>47</em> credits</p>
+`;
+  expect (htmlStatement (invoices[0], plays)).toBe (expected);
 });
